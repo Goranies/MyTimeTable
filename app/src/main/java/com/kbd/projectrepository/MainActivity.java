1e809205064f419f048e459ba6a0a56b930929f9
carefulseop510 <wndtjq0510@naver.com>
2021-08-05 02:19:57 +0900

    Add btn connect to lobby

--- app/src/main/java/com/kbd/projectrepository/MainActivity.java
+++ app/src/main/java/com/kbd/projectrepository/MainActivity.java
@@ -2,7 +2,9 @@ package com.kbd.projectrepository;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_ProjectRepository);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething(View view) {
+        Intent i1 = new Intent(this, LobbyActivity.class);
+        startActivity(i1);
     }
 }
