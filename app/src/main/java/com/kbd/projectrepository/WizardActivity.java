4f9abb5782916ce09b21cb04d8006eca15f2385f
byungmeo <wlsrlfvkr@gmail.com>
2021-08-04 15:41:02 +0900

    엑티비티 생성

---
+++ app/src/main/java/com/kbd/projectrepository/WizardActivity.java
@@ -0,0 +1,14 @@
+package com.kbd.projectrepository;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WizardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wizard);
+    }
+}
