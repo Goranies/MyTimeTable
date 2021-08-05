13f94fa94b2bf26b2724b078dbf657a9fb0f2103
MadeByZiNo <38098073+MadeByZiNo@users.noreply.github.com>
2021-08-05 14:54:03 +0900

    Menu

--- app/src/main/java/com/kbd/projectrepository/LobbyActivity.java
+++ app/src/main/java/com/kbd/projectrepository/LobbyActivity.java
@@ -1,8 +1,12 @@
 package com.kbd.projectrepository;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class LobbyActivity extends AppCompatActivity {
 
@@ -11,4 +15,28 @@ public class LobbyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.lobbybar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if(item.getItemId()==R.id.lobby_mitem_GotoWizard)
+        {
+            Intent intent = new Intent(this,TimeInputActivity.class);
+            startActivity(intent);
+        }
+        else if(item.getItemId()==R.id.lobby_mitem_GotoInput)
+        {
+            Intent intent = new Intent(this,WizardActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
 }
