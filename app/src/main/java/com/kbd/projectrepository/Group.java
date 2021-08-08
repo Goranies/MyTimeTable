2b30ab9e23414fc0e593caa5ab1d295c55d49924
byungmeo <wlsrlfvkr@gmail.com>
2021-08-09 02:52:29 +0900

    Wizard/add ViewHolder ClickEvent & add Footer Button ETC...

--- app/src/main/java/com/kbd/projectrepository/Group.java
+++ app/src/main/java/com/kbd/projectrepository/Group.java
@@ -1,17 +1,8 @@
 package com.kbd.projectrepository;
 
 public class Group {
-    private String groupName;
 
-    public Group(String groupName) {
-        this.groupName = groupName;
+    public Group() {
     }
 
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
 }
--- app/src/main/java/com/kbd/projectrepository/WizardActivity.java
+++ app/src/main/java/com/kbd/projectrepository/WizardActivity.java
@@ -1,7 +1,6 @@
 package com.kbd.projectrepository;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,9 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class WizardActivity extends AppCompatActivity {
-//    private InputMethodManager imm;
-//    private EditText groupName;
-
     private RecyclerView groupRecycler;
     private ArrayList<Group> groupArrayList;
 
@@ -22,21 +18,19 @@ public class WizardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wizard);
 
         initGroupRecycler();
-//        initGroup();
     }
 
     public void initGroupRecycler() {
         groupRecycler = findViewById(R.id.wizard_recycler);
 
         groupArrayList = new ArrayList<>();
-        groupArrayList.add(new Group("Group 1"));
-        groupArrayList.add(new Group("Group 2"));
-        groupArrayList.add(new Group("Group 3"));
-        groupArrayList.add(new Group("Group 4"));
-        groupArrayList.add(new Group("Group 5"));
-        groupArrayList.add(new Group("Group 6"));
-        groupArrayList.add(new Group("Group 7"));
-        groupArrayList.add(new Group("Group 8"));
+        groupArrayList.add(new Group());
+        groupArrayList.add(new Group());
+        groupArrayList.add(new Group());
+        groupArrayList.add(new Group());
+        groupArrayList.add(new Group());
+        groupArrayList.add(new Group());
+        groupArrayList.add(new Group());
 
         GroupAdapter groupAdapter = new GroupAdapter(this, groupArrayList);
 
@@ -44,41 +38,5 @@ public class WizardActivity extends AppCompatActivity {
 
         groupRecycler.setLayoutManager(linearLayoutManager);
         groupRecycler.setAdapter(groupAdapter);
-    }
-
-    public void initGroup() {
-//        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-//        groupName = findViewById(R.id.wizard_card_editText_groupName);
-//
-//        groupName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if(hasFocus) {
-//                    imm.showSoftInput(v, 0);
-//                } else {
-//                    groupName.setFocusable(false);
-//                }
-//            }
-//        });
-//
-//        groupName.setOnKeyListener(new View.OnKeyListener() {
-//            @Override
-//            public boolean onKey(View v, int keyCode, KeyEvent event) {
-//                if(event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-//                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-//                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-    }
-
-    public void clickButton(View view) {
-//        if(view.getId() == R.id.wizard_card_button_editName) {
-//            groupName.setFocusable(true);
-//            groupName.setFocusableInTouchMode(true);
-//            groupName.requestFocus();
-//        }
     }
 }
